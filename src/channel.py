ebcd3e1bdf2eaa371dd6c7eba402a1adb555c6bc
Artem Osipov <tema.osipov@mail.ru>
2023-07-05 17:04:24 +0300

    Установлен ключ в переменные среды. Проводится инициализация по id. Создан метод print_info

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,22 @@
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    API_KEY = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'
+    channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(self.channel)
+#
