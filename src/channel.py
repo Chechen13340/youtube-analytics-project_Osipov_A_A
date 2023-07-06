f3a4d166180b0f61dae7d6a6c24a4cb0952bcde9
Artem Osipov <tema.osipov@mail.ru>
2023-07-06 10:44:53 +0300

    внес измнения согласно комментариям. Использется channel_id

--- src/channel.py
+++ src/channel.py
@@ -4,17 +4,14 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
-    API_KEY = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=API_KEY)
-    channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'
-    channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала.
         Дальше все данные будут подтягиваться по API."""
+        API_KEY = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
         self.channel_id = channel_id
-
+        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
